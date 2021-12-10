e72a2c03cc88dc5d7e93006cb576f6361868a198
Athul <athulsprakash@gmail.com>
2021-12-10 21:40:31 +0530

    bug

--- index.js
+++ index.js
@@ -7,18 +7,15 @@ async function fetch(){
     res.data.map( d => d.population>=84497 && D.push([d.latlng[0]/r,d.latlng[1]/r]) )
 }
 fetch().then(()=>{
-    D = D.slice(0,20)
     let sum = 0
-    for(let i = 0; i<D.length; i++)
-        for(let j = i+1; j<D.length; j++){
-            const dx = D[j][0]-D[i][0]
-            const dy = D[j][1]-D[i][1]
-            const calc = Math.pow(Math.sin(dx/2),2)  
+    for(let i = 0; i<20; i++)
+        for(let j = i+1; j<20; j++){
+            const calc = Math.sin((D[j][0]-D[i][0])/2)**2 
                 + Math.cos(D[i][0]) * Math.cos(D[j][0])
-                * Math.pow(Math.sin(dy/2),2)
+                * Math.sin((D[j][1]-D[i][1])/2)**2
             let distance = 2 * 6731 * Math.asin(Math.sqrt(calc))
             distance = Math.round(distance * 1e2)/1e2
-            sum += distance  
+            sum += distance
         }
     console.log(Math.round(sum * 1e2)/1e2)
 })
